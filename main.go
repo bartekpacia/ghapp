77be822fb3e519b8e2d7fea7f352a3e1f672f703
Bartek Pacia <barpac02@gmail.com>
2024-07-14 17:10:24 +0100

    minor fixes

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"log/slog"
 	"net/http"
 )
 
@@ -22,7 +21,6 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	slog.Info("request received!", slog.String("path", r.URL.Path))
 	fmt.Fprintln(w, "hello world")
 }
 
