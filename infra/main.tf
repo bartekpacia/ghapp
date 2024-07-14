96db160cb21ee792a416ef8040fe46cf0c256f9c
Bartek Pacia <barpac02@gmail.com>
2024-07-14 19:03:38 +0100

    cloud run terraform: expose to all users

--- infra/main.tf
+++ infra/main.tf
@@ -42,6 +42,7 @@ resource "google_project" "default" {
 
 
 resource "google_cloud_run_service" "default" {
+  project  = google_project.default.project_id
   name     = "main-cloud-run-service"
   location = var.region
 
@@ -57,5 +58,22 @@ resource "google_cloud_run_service" "default" {
     percent         = 100
     latest_revision = true
   }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
 
