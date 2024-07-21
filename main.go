f712bf30e4560f2ace3d00288dc6237115da2963
Bartek Pacia <barpac02@gmail.com>
2024-07-21 01:02:51 +0100

    add some webhook handling code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -14,6 +15,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", index)
 	mux.HandleFunc("POST /webhook", handleWebhook)
+	mux.HandleFunc("POST /check-runs", handleCheckRuns)
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
@@ -22,18 +24,73 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	slog.Info("request received!", slog.String("path", r.URL.Path))
+	slog.Info("request received", slog.String("path", r.URL.Path))
 	fmt.Fprintln(w, "hello world")
 }
 
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	l := slog.With("path", r.URL.Path)
+
 	// Print request information, such as headers and body
-	fmt.Println("Request Headers: ", r.Header)
+	l.Info("new request")
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println("Error reading body: ", err)
+		l.Error("error reading body", slog.Any("error", err))
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("Error reading body: %v", err)))
 		return
-	} else {
-		fmt.Println("Request Body: ", string(body))
 	}
+
+	var payload map[string]interface{}
+	err = json.Unmarshal([]byte(body), &payload)
+	if err != nil {
+		l.Error("error reading body", slog.Any("error", err))
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error reading body: %v", err)
+		return
+	}
+
+	// Check type of webhook event
+	eventType := r.Header.Get("X-GitHub-Event")
+	switch eventType {
+	case "installation":
+		// https://docs.github.com/en/webhooks/webhook-events-and-payloads?actionType=created#installation
+		if payload["action"] == "created" {
+			repository := payload["repository"].(map[string]interface{})
+			fullName := repository["full_name"].(string)
+			l.Info("installation created", slog.Any("repo", fullName))
+		}
+	case "check_suite":
+		// https://docs.github.com/en/webhooks/webhook-events-and-payloads?actionType=requested#check_suite
+		if payload["action"] == "requested" {
+			repository := payload["repository"].(map[string]interface{})
+			fullName := repository["full_name"].(string)
+
+			checkSuite := payload["check_suite"].(map[string]interface{})
+			headSHA := checkSuite["head_sha"].(string)
+			l.Info("check suite requested", slog.Any("repo", fullName), slog.String("commit", headSHA))
+		}
+	default:
+		l.Error("unknown event type", slog.String("type", eventType))
+	}
+}
+
+func handleCheckRuns(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println("Request Headers: ", r.Header)
+	// reqBody, err := io.ReadAll(r.Body)
+	// if err != nil {
+	// 	fmt.Println("Error reading body: ", err)
+	// }
+	//
+	// params := r.URL.Query()
+	// owner := params.Get("owner")
+	// repo := params.Get("repo")
+	// name := params.Get("name")
+	//
+	// body := []byte(`{
+	// 	"name": "linter",
+	// 	"head_sha": "1234567890abcdef",
+	// }`)
+	//
+	// http.NewRequest("POST", "https://api.github.com/repos/:owner/:repo/check-runs", body)
 }
